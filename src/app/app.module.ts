99dde4e2088f5d408600d16e53f80f2b287e9772
Houssem Ben Ali <houssembenali90@gmail.com>
2023-04-09 05:56:05 +0200

    * add bootstrap * uptade gitignore * page-login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageLoginComponent } from './pages/src/app/pages/page-login/page-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    PageLoginComponent  ],
   imports: [
     BrowserModule,
     AppRoutingModule
